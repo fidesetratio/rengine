1b2ed727bc397e20709caa0f844896862a0aba99
Patar.Tambunan <Patar.Tambunan@Patarlaptop.SINARMASMSIGLIFE.CO.ID>
2020-12-28 22:43:36 +0700

    commit go

--- src/main/java/com/app/config/TaskPluginExecutorConfig.java
+++ src/main/java/com/app/config/TaskPluginExecutorConfig.java
@@ -1,5 +1,21 @@
 package com.app.config;
 
-public class TaskPluginExecutorConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@Configuration
+public class TaskPluginExecutorConfig {
+	@Bean(name="processExecutor")
+    public TaskExecutor workExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("Async-");
+        threadPoolTaskExecutor.setCorePoolSize(3);
+        threadPoolTaskExecutor.setMaxPoolSize(3);
+        threadPoolTaskExecutor.setQueueCapacity(600);
+        threadPoolTaskExecutor.afterPropertiesSet();
+     
+        return threadPoolTaskExecutor;
+    }
 }
--- src/main/java/com/app/services/TaskPluginServices.java
+++ src/main/java/com/app/services/TaskPluginServices.java
@@ -1,4 +1,5 @@
-package com.app.controller;
+package com.app.services;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -10,7 +11,6 @@ import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -18,21 +18,13 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
-import com.app.model.QueryModel;
-import com.app.model.Table;
 import com.app.plugin.core.TaskTable;
-import com.app.services.QueryServices;
-import com.app.services.TaskPluginServices;
 
-@RestController
-@RequestMapping("/plugin")
-public class PluginRest {
+@Service
+public class TaskPluginServices {
 	
 	@Autowired
 	private QueryServices queryServices;
@@ -50,38 +42,10 @@ public class PluginRest {
 	@Autowired
 	private JobLauncher jobLauncher;
 	
-	@Autowired
-	private TaskPluginServices taskPluginServices;
-
-	@PostMapping(path = "/query", consumes = "application/json", produces = "application/json")
-	public Table query(@RequestBody QueryModel qmodel) {
-	    Table table = new Table();
-		String query = qmodel.getQuery();
-		table = queryServices.query(query);
-		return table;
-		
-	}
-
-	@GetMapping("/executeTask")
-    public String executeTask() {
-		
-		    JobParametersBuilder paramsBuilder = new JobParametersBuilder();
-	        paramsBuilder.addString("selectQuery", "select rownum as number1,a.reg_spaj");
-	        paramsBuilder.addString("fromQuery", "FROM eka.mst_policy a");
-	        paramsBuilder.addString("whereQuery", "where 1=1");	        
-	        taskPluginServices.executeTask(paramsBuilder);
-	        
-	        return "executeTask";
-		
-	}
 	
-	@GetMapping("/executeTask2")
-    public String executeTask2() {
-		 	/*
-			 * ItemReader reader = (ItemReader)context.getBean("pagingItemReaderBean");
-			 * ItemWriter writer = (ItemWriter)context.getBean("pagingItemWriterBean");
-			 */
-		  ItemReader reader = (ItemReader)context.getBean("pagingItemReaderBean");
+	@Async("processExecutor")
+	public void executeTask( JobParametersBuilder paramsBuilder) {
+		 ItemReader reader = (ItemReader)context.getBean("pagingItemReaderBean");
 		  ItemWriter writer = (ItemWriter)context.getBean("pagingItemWriterBean");
 		  Step step = stepBuilderFactory.get("orderStep1").<TaskTable, TaskTable> chunk(100).reader(reader).writer(writer).build();
 		  Flow oneFlowOnly = new FlowBuilder<Flow>("singleFlow").
@@ -93,14 +57,8 @@ public class PluginRest {
 	                .start(oneFlowOnly)
 	                .end()
 	                .build();
-
-	        JobParametersBuilder paramsBuilder = new JobParametersBuilder();
-	        paramsBuilder.addString("selectQuery", "select rownum as number1,a.reg_spaj");
-	        paramsBuilder.addString("fromQuery", "FROM eka.mst_policy a");
-	        paramsBuilder.addString("whereQuery", "where 1=1");
-	        
-	        
-	        try {
+		  
+		  try {
 				JobExecution execution = jobLauncher.run(job,paramsBuilder.toJobParameters());
 			} catch (JobExecutionAlreadyRunningException e) {
 				// TODO Auto-generated catch block
@@ -115,11 +73,6 @@ public class PluginRest {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		  
-			/*
-			 * Step step = stepBuilderFactory.get("orderStep1").<String, String> chunk(1)
-			 * .reader(reader1(null)) .writer(writer1(null)).build();
-			 */
-	    return "executeTask";
-    }
+	}
+
 }
